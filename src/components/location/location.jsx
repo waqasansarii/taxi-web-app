a150c520336c01e84a7b1732ba5dc7cb47bee46d
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-01-31 21:12:23 +0500

    changed folder name

---
+++ src/components/location/location.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './location.css'
+import { Link } from 'react-router-dom'
+
+
+const Location = () => {
+    return (
+        <div className='location_container'>
+            <div className='location_div'>
+                <div className='input_and_map'>
+                    <Link className='location_link' to='/location'>
+
+                        <FontAwesomeIcon className='map_marker' icon={faMapMarkerAlt} />
+                        <span style={{marginLeft:'10px',fontWeight:'bold',color:'grey'}}>
+                         Enter Your Pickup Location
+                            
+                        </span>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default Location
